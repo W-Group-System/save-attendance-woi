51c7d411d9604262f4a6e1d99fdbedc849eac791
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2024-03-20 15:56:05 +0800

    Done

--- app/Console/Commands/get_attendance_hk.php
+++ app/Console/Commands/get_attendance_hk.php
@@ -54,11 +54,11 @@ class get_attendance_hk extends Command
             if($response->id)
             {
 
-                $attendances = Vms::where('timpue_input','!=',null)->where('last_id','>=',$response->id)->orderBy('id','desc')->get()->take(200);
+                $attendances = Vms::where('timpue_input','!=',null)->where('last_id','>=',$response->id)->orderBy('id','asc')->get()->take(200);
             }
             else
             {
-                $attendances = Vms::where('date_input','>=',date('Y-m-d',strtotime('2024-02-15')))->orderBy('id','desc')->get()->take(200);
+                $attendances = Vms::where('date_input','>=',date('Y-m-d',strtotime('2024-02-15')))->orderBy('id','asc')->get()->take(200);
             }
             $requestContent = [
                 'headers' => [
